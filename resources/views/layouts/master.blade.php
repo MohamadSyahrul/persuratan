ce9aaa8f6fc10307e79adc46e361cc8b0a78ea88
MohamadSyahrul <mochamadsahrul28@gmail.com>
2022-06-27 23:43:05 +0700

    template

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    @include('layouts.style')
+    @stack('style')
+</head>
+
+<body>
+    <div id="app">
+        <div id="sidebar" class="active">
+            @include('layouts.sidebar')
+        </div>
+        <div id="main">
+            <header class="mb-3">
+                <a href="#" class="burger-btn d-block d-xl-none">
+                    <i class="bi bi-justify fs-3"></i>
+                </a>
+            </header>
+            @yield('content')
+            <footer>
+                <div class="footer clearfix mb-0 text-muted">
+                    <div class="float-start">
+                        <p>2021 &copy; Mazer</p>
+                    </div>
+                    <div class="float-end">
+                        <p>Crafted with <span class="text-danger"><i class="bi bi-heart"></i></span> by <a
+                                href="https://saugi.me">Saugi</a></p>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    </div>
+
+    <script src="{{asset('assets/js/app.js')}}"></script>
+    @stack('script')
+</body>
+
+</html>
