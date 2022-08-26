b58d43083beb8be25bc0362e9c9d73039de90d28
MohamadSyahrul <mochamadsahrul28@gmail.com>
2022-08-27 00:52:44 +0700

    filter

--- app/Http/Controllers/LaporanController.php
+++ app/Http/Controllers/LaporanController.php
@@ -2,16 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use App\Models\SuratKeluar;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 
 class LaporanController extends Controller
 {
-    public function index(){
-        $pagename = "Laporan Surat";
-        $laporan = SuratKeluar::all();
-        return view('pages.laporan', compact('pagename', 'laporan'));
+    public function index(Request $request){
+        // $pagename = "Laporan Surat";
+        // // $laporan = SuratKeluar::all();
+        // // return view('pages.laporan', compact('pagename', 'laporan'));
+        // if(request()->ajax())
+        // {
+        //  if(!empty($request->from_date))
+        //  {
+          $data = DB::table('surat_keluars')
+            ->whereBetween('created_at', array($request->from_date, $request->to_date))
+            ->get();
+            return response()->json($data, 200);
+        //  }
+        //  else
+        //  {
+        //   $data = DB::table('surat_keluars')
+        //     ->get();
+        //  }
+        //  return datatables()->of($data)->make(true);
+        // }
+        // return view('pages.laporan', compact('pagename'));
     }
 
     public function laporansm(){
--- resources/views/pages/laporan.blade.php
+++ resources/views/pages/laporan.blade.php
@@ -2,9 +2,18 @@
 @push('style')
 {{-- <link rel="stylesheet" href="{{asset('assets/css/pages/datatables.css')}}"> --}}
 <link rel="stylesheet" href="{{asset('assets/css/pages/fontawesome.css')}}">
-<link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
+{{-- <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css"> --}}
 {{-- <link rel="stylesheet" href="https://cdn.datatables.net/fixedheader/3.2.3/css/fixedHeader.dataTables.min.css"> --}}
-<link rel="stylesheet" href="https://cdn.datatables.net/datetime/1.1.2/css/dataTables.dateTime.min.css">
+{{-- <link rel="stylesheet" href="https://cdn.datatables.net/datetime/1.1.2/css/dataTables.dateTime.min.css"> --}}
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+{{-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" /> --}}
+<script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>  
+<link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css" />
+{{-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script> --}}
+<link rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.8.0/css/bootstrap-datepicker.css" />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.8.0/js/bootstrap-datepicker.js"></script>
 @endpush
 @section('content')
 <div class="page-heading">
@@ -31,27 +40,38 @@
 
             </div>
             <div class="card-body">
-                <table cellspacing="5" cellpadding="5" border="0">
-                    <tbody><tr>
-                        <td>Dari Tanggal:</td>
-                        <td><input type="text" id="min" name="min"></td>
-                    </tr>
-                    <tr>
-                        <td>Sampai Tanggal:</td>
-                        <td><input type="text" id="max" name="max"></td>
-                    </tr>
+                <div class="row input-daterange">
+                    <div class="col-md-4">
+                        <input type="text" name="from_date" id="from_date" class="form-control" placeholder="From Date" readonly />
+                    </div>
+                    <div class="col-md-4">
+                        <input type="text" name="to_date" id="to_date" class="form-control" placeholder="To Date" readonly />
+                    </div>
+                    <div class="col-md-4">
+                        <button type="button" name="filter" id="filter" class="btn btn-primary">Filter</button>
+                        <button type="button" name="refresh" id="refresh" class="btn btn-default">Refresh</button>
+                    </div>
+                </div>
+                {{-- <table cellspacing="5" cellpadding="5" border="0">
+                    <tbody>
+                        <tr>
+                            <td>Dari Tanggal:</td>
+                            <td><input type="text" id="min" name="min"></td>
+                        </tr>
+                        <tr>
+                            <td>Sampai Tanggal:</td>
+                            <td><input type="text" id="max" name="max"></td>
+                        </tr>
                     </tbody>
-                </table>
+                </table> --}}
                 <table id="example" class="table display" style="width:100%">
                     <thead>
                         <tr>
                             <th>No Surat</th>
                             <th>Nama Pembuat</th>
-                            <th>Tanggal Surat</th>
-                            <th>Dokumen</th>
                         </tr>
                     </thead>
-                    <tbody>
+                    {{-- <tbody>
                         @foreach ($laporan as $item)
                         <tr>
                             <td>{{$item->no_surat}}</td>
@@ -59,19 +79,19 @@
                             <td>{{$item->tgl_surat}}</td>
                             <td>
                                 @if (Auth::user()->level == 'kepalabiro')
-                                    <a href="{{route('downloadkepalabiro', $item->dokumen)}}">
-                                        {{$item->dokumen}}
-                                    </a>                                    
+                                <a href="{{route('downloadkepalabiro', $item->dokumen)}}">
+                                    {{$item->dokumen}}
+                                </a>
                                 @endif
                                 @if (Auth::user()->level == 'pimpinan')
-                                    <a href="{{route('downloadpimpinan', $item->dokumen)}}">
-                                        {{$item->dokumen}}
-                                    </a>                                    
+                                <a href="{{route('downloadpimpinan', $item->dokumen)}}">
+                                    {{$item->dokumen}}
+                                </a>
                                 @endif
                             </td>
                         </tr>
                         @endforeach
-                    </tbody>
+                    </tbody> --}}
                 </table>
             </div>
         </div>
@@ -81,13 +101,13 @@
 </div>
 @endsection
 @push('script')
-<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+{{-- <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
 <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.2/moment.min.js"></script>
-<script src="https://cdn.datatables.net/datetime/1.1.2/js/dataTables.dateTime.min.js"></script>
+<script src="https://cdn.datatables.net/datetime/1.1.2/js/dataTables.dateTime.min.js"></script> --}}
 
-<script>
+{{-- <script>
     var minDate, maxDate;
  
  // Custom filtering function which will search data in column four between two values
@@ -126,6 +146,61 @@
          table.draw();
      });
  });
+</script> --}}
+
+<script>
+    $(document).ready(function () {
+        $('.input-daterange').datepicker({
+            todayBtn: 'linked',
+            format: 'yyyy-mm-dd',
+            autoclose: true
+        });
+
+        load_data();
+
+        function load_data(from_date = '', to_date = '') {
+            $('#example').DataTable({
+                processing: true,
+                serverSide: true,
+                ajax: {
+                    url: '{{ route("laporan") }}',
+                    data: {
+                        from_date: from_date,
+                        to_date: to_date
+                    }
+                },
+                columns: [{
+                        data: 'no_surat',
+                        name: 'no_surat'
+                    },
+                    {
+                        data: 'tgl_surat',
+                        name: 'tgl_surat'
+                    }
+                ]
+            });
+        }
+
+        $('#filter').click(function () {
+            var from_date = $('#from_date').val();
+            var to_date = $('#to_date').val();
+            if (from_date != '' && to_date != '') {
+                $('#example').DataTable().destroy();
+                load_data(from_date, to_date);
+            } else {
+                alert('Both Date is required');
+            }
+        });
+
+        $('#refresh').click(function () {
+            $('#from_date').val('');
+            $('#to_date').val('');
+            $('#example').DataTable().destroy();
+            load_data();
+        });
+
+    });
+
 </script>
 
 @endpush
